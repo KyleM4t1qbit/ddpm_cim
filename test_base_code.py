abaacd2884ec7bf172ce487d0de52592993bd98c
Kyle Mills <kyle.mills@1qbit.com>
2022-02-01 18:14:06 -0500

    added basic test

--- test_base_code.py
+++ test_base_code.py
@@ -1,6 +1,11 @@
 import pytest
 
+def test_base_code_import():
+    from MF_CIM_weighted_vanilla import sde_MF_CIM_weighted_vanilla
+    
 def test_base_code():
     from MF_CIM_weighted_vanilla import sde_MF_CIM_weighted_vanilla
+    mu_tilde = sde_MF_CIM_weighted_vanilla(dim=10, pmax=3.2, iter_n=100)
+
 
 
